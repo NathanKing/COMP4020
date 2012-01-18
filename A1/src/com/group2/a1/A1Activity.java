44af71031d5fc0a52331cafa099236df8fc7f7ef
Nathan King <nathanking007@gmail.com>
2012-01-17 20:06:01 -0600

    test

---
+++ A1/src/com/group2/a1/A1Activity.java
@@ -0,0 +1,13 @@
+package com.group2.a1;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class A1Activity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
---
+++ A2/src/com/group2/a2/A2Activity.java
@@ -0,0 +1,13 @@
+package com.group2.a2;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class A2Activity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
